5415c2fac35633bbd3315e87714b3327d8904f4e
D.T <dancingonmoon@msn.com>
2024-06-25 15:30:27 +0800

    ExcelColumn Translation update: 安装pyecharts后,出现pandas,numpyé版本降低;环境调整

--- Translation/ExcelColumnTranslate.py
+++ Translation/ExcelColumnTranslate.py
@@ -18,12 +18,15 @@ class translate_engine(Enum):
 
 # https://fanyi-api.baidu.com/api/trans/product/desktop?req=developer
 
-def config_read(config_path, section='DingTalkAPP_chatGLM', option1='Client_ID', option2=None):
+
+def config_read(
+    config_path, section="DingTalkAPP_chatGLM", option1="Client_ID", option2=None
+):
     """
     option2 = None 时,仅输出第一个option1的值; 否则输出section下的option1与option2两个值
     """
     config = configparser.ConfigParser()
-    config.read(config_path, encoding='utf-8')
+    config.read(config_path, encoding="utf-8")
     option1_value = config.get(section=section, option=option1)
     if option2 is not None:
         option2_value = config.get(section=section, option=option2)
@@ -33,29 +36,36 @@ def config_read(config_path, section='DingTalkAPP_chatGLM', option1='Client_ID',
 
 
 # Generate salt and sign
-def make_md5(s, encoding='utf-8'):
+def make_md5(s, encoding="utf-8"):
     return md5(s.encode(encoding)).hexdigest()
 
 
 # 定义百度翻译ＡＰＩ函数
-def BaiduTranslateAPI(text, appid, appkey, from_lang='auto', to_lang='en'):
+def BaiduTranslateAPI(text, appid, appkey, from_lang="auto", to_lang="en"):
     """
     baidu_appid: 百度openAPI baidu_appid
     baidu_appkey: 百度openAPI baidu_appkey
     For list of language codes, please refer to `https://api.fanyi.baidu.com/doc/21
     """
     salt = random.randint(32768, 65536)
-    s = ''.join([appid, text, str(salt), appkey])
+    s = "".join([appid, text, str(salt), appkey])
     sign = make_md5(s)
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    payload = {'appid': appid, 'q': text, 'from': from_lang, 'to': to_lang, 'salt': salt, 'sign': sign}
-    endpoint = 'http://api.fanyi.baidu.com'
-    path = '/api/trans/vip/translate'
-    url = ''.join([endpoint, path])
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    payload = {
+        "appid": appid,
+        "q": text,
+        "from": from_lang,
+        "to": to_lang,
+        "salt": salt,
+        "sign": sign,
+    }
+    endpoint = "http://api.fanyi.baidu.com"
+    path = "/api/trans/vip/translate"
+    url = "".join([endpoint, path])
     r = requests.post(url, params=payload, headers=headers)
     result = r.json()
     try:
-        trans_result = result['trans_result'][0]['dst']
+        trans_result = result["trans_result"][0]["dst"]
         return trans_result
     except Exception as e:
         return result
@@ -65,7 +75,9 @@ def BaiduTranslateAPI(text, appid, appkey, from_lang='auto', to_lang='en'):
 
 
 # 定义函数:DataFrame单列遍历,调用百度翻译API,并生成翻译后的DataFrame
-def DFColumnTranslate_baidu(DFColumn, appid, appkey, from_lang: str = 'auto', to_lang: str = 'en'):
+def DFColumnTranslate_baidu(
+    DFColumn, appid, appkey, from_lang: str = "auto", to_lang: str = "en"
+):
     """
     百度翻译API下的单列文本翻译, 输入DataFrame列,翻译后,返回DataFrame列,包括列名翻译;空白文本自动跳过;
     DFColum: DataFrame单列
@@ -74,8 +86,10 @@ def DFColumnTranslate_baidu(DFColumn, appid, appkey, from_lang: str = 'auto', to
     """
     Translate = []
     for value in DFColumn.values:
-        if value[0] != '':
-            Translate.append(BaiduTranslateAPI(str(value[0]), appid, appkey, from_lang, to_lang))
+        if value[0] != "":
+            Translate.append(
+                BaiduTranslateAPI(str(value[0]), appid, appkey, from_lang, to_lang)
+            )
         else:
             Translate.append(value)
     Column_name = DFColumn.keys()[0]
@@ -86,8 +100,13 @@ def DFColumnTranslate_baidu(DFColumn, appid, appkey, from_lang: str = 'auto', to
     return Translate
 
 
-def DFColumnTranslate_MS(DFColumn, ms_subscribeKey, dynamic_dict: dict = False, from_lang: str = None,
-                         to_lang: str = 'en'):
+def DFColumnTranslate_MS(
+    DFColumn,
+    ms_subscribeKey,
+    dynamic_dict: dict = False,
+    from_lang: str = None,
+    to_lang: str = "en",
+):
     """
     微软翻译API下的单列文本翻译, 输入DataFrame列,翻译后,返回DataFrame列,包括列名翻译;空白文本自动跳过;
     DFColum: DataFrame单列
@@ -98,25 +117,35 @@ def DFColumnTranslate_MS(DFColumn, ms_subscribeKey, dynamic_dict: dict = False, 
     DFColumn_list = DFColumn.values.tolist()
     DFColumn_list = [l[0] for l in DFColumn_list]
 
-    Translate, response = MStranslation_dynamicDictionary_API(DFColumn_list, dynamic_dict=dynamic_dict,
-                                                              lang_in=from_lang,
-                                                              lang_out=to_lang, subscription_key=ms_subscribeKey)
-    if 'error' in response:
+    Translate, response = MStranslation_dynamicDictionary_API(
+        DFColumn_list,
+        dynamic_dict=dynamic_dict,
+        lang_in=from_lang,
+        lang_out=to_lang,
+        subscription_key=ms_subscribeKey,
+    )
+    if "error" in response:
         return response
 
     Column_name = DFColumn.keys()[0]
     if isinstance(Column_name, str):
-        Column_name, response = MStranslation_dynamicDictionary_API(Column_name, dynamic_dict=dynamic_dict,
-                                                                    lang_in=from_lang,
-                                                                    lang_out=to_lang, subscription_key=ms_subscribeKey)
-        if 'error' in response:
+        Column_name, response = MStranslation_dynamicDictionary_API(
+            Column_name,
+            dynamic_dict=dynamic_dict,
+            lang_in=from_lang,
+            lang_out=to_lang,
+            subscription_key=ms_subscribeKey,
+        )
+        if "error" in response:
             return response
 
     Translate = pd.DataFrame(Translate, columns=[Column_name])
     return Translate
 
 
-def Write2Excel(DFColumn, Write2Path, Write2Sheet, Write2Row=0, Write2Col=0, Writeheader=True):
+def Write2Excel(
+    DFColumn, Write2Path, Write2Sheet, Write2Row=0, Write2Col=0, Writeheader=True
+):
     """
     将DataFrame追加写入已经存在的Excel,写入指定的sheet, 从指定的行列号开始写完整的DataFrame块;
     DataFrame: 写入的DataFrame
@@ -126,15 +155,36 @@ def Write2Excel(DFColumn, Write2Path, Write2Sheet, Write2Row=0, Write2Col=0, Wri
     Write2Col: 写入的起始列,默认为0,
     Writeheader: bool类型, 是否写入标题行,默认为True;
     """
-    with pd.ExcelWriter(Write2Path, mode='a', engine="openpyxl", if_sheet_exists='overlay') as writer:
-        DFColumn.to_excel(writer, Write2Sheet, startrow=Write2Row, startcol=Write2Col, index=False, header=Writeheader)
+    with pd.ExcelWriter(
+        Write2Path, mode="a", engine="openpyxl", if_sheet_exists="overlay"
+    ) as writer:
+        DFColumn.to_excel(
+            excel_writer=writer,
+            sheet_name=Write2Sheet,
+            startrow=Write2Row,
+            startcol=Write2Col,
+            index=False,
+            header=Writeheader,
+        )
 
 
-def ExcelColumnTranslate(translate_engine: translate_engine = 1, ms_subscribeKey=None, baidu_appid=None,
-                         baidu_appkey=None, from_lang: Union[str, None] = 'auto', to_lang='en', ExcelPath: str = None,
-                         readSheet=None, readHeader: int = None, readStartRow: Union[int, None] = 1,
-                         readCol: Union[int, list[str]] = None, nrows: int = None,
-                         write2Sheet: str = None, write2Row: int = 2, write2Col: int = 1):
+def ExcelColumnTranslate(
+    translate_engine: translate_engine = 1,
+    ms_subscribeKey=None,
+    baidu_appid=None,
+    baidu_appkey=None,
+    from_lang: Union[str, None] = "auto",
+    to_lang="en",
+    ExcelPath: str = None,
+    readSheet:str=None,
+    readHeader: int = None,
+    readStartRow: Union[int, None] = 1,
+    readCol: Union[int, list[int]] = None,
+    nrows: int = None,
+    write2Sheet: str = None,
+    write2Row: int = 2,
+    write2Col: int = 1,
+):
     """
     Excel中读取单列,或者block,并调用百度翻译API,翻译后,写入已经存在的Excel,写入指定的sheet, 从指定的行列号追加写入完整的列或者块;
     translate_engine: 枚举变量: 缺省为microsoft翻译引擎(1), 其次为baidu翻译引擎(2)
@@ -163,7 +213,7 @@ def ExcelColumnTranslate(translate_engine: translate_engine = 1, ms_subscribeKey
         return
     # 检查xlsx文件是否已经有临时文件打开,从而判断是否EXCEL文件已经打开:
     dirname, filename = os.path.split(ExcelPath)
-    filename = "~$".join(['', filename])
+    filename = "~$".join(["", filename])
     temp_path = os.path.join(dirname, filename)
     if os.path.exists(temp_path):
         print(f"{ExcelPath},文件已经打开,禁止写入.")
@@ -192,54 +242,77 @@ def ExcelColumnTranslate(translate_engine: translate_engine = 1, ms_subscribeKey
     else:
         readCol = [readCol - 1]
 
-    ExcelData = pd.read_excel(ExcelPath, readSheet, header=readHeader, skiprows=skiprows, usecols=readCol,
-                              nrows=nrows, na_values='')
+    ExcelData = pd.read_excel(
+        ExcelPath,
+        readSheet,
+        header=readHeader,
+        skiprows=skiprows,
+        usecols=readCol,
+        nrows=nrows,
+        na_values="",
+    )
     if translate_engine == translate_engine.baidu:
-        TransData = DFColumnTranslate_baidu(ExcelData, baidu_appid, baidu_appkey, from_lang, to_lang)
+        TransData = DFColumnTranslate_baidu(
+            ExcelData, baidu_appid, baidu_appkey, from_lang, to_lang
+        )
     elif translate_engine == translate_engine.microsoft:
-        TransData = DFColumnTranslate_MS(ExcelData, ms_subscribeKey=ms_subscribeKey, from_lang=from_lang,
-                                         to_lang=to_lang)
-        if 'error' in TransData:
-            print(TransData['error'])
+        TransData = DFColumnTranslate_MS(
+            ExcelData,
+            ms_subscribeKey=ms_subscribeKey,
+            from_lang=from_lang,
+            to_lang=to_lang,
+        )
+        if "error" in TransData:
+            print(TransData["error"])
             return
     else:
         print("translate_engine参数错误")
         return
 
-    Write2Excel(TransData, ExcelPath, write2Sheet, write2Row, write2Col - 1, writeheader)  # -2 因为index与行号差1
+    Write2Excel(
+        TransData, ExcelPath, write2Sheet, write2Row, write2Col - 1, writeheader
+    )  # -2 因为index与行号差1
     print(f"翻译并写入完成! 请查看{ExcelPath}.{write2Sheet}")
 
 
 if __name__ == "__main__":
     # 读取需要翻译的Excel文件,装入DataFrame
-    from_lang = 'auto'  # 百度: 'auto', 微软: None; 自动辨识语言;当双语混杂时,需要指定语言,否则会有漏译情况;
-    to_lang = 'en'  # 百度:( 中文: zh;文言文: wyw;日本: jp; --> 伊朗语: ir; 波斯语);微软: "zh-Hans"中文简体
-    ExcelPath = r"E:/Working Documents/Eastcom/Russia/Igor/专网/LeoTelecom/发货测试验收/BOM Packing List.xlsx"
-    readSheet = 'Sheet1'
-    readHeader = 1
+    from_lang_ms = "zh-Hans"  # 百度: 'auto', 微软: None; 自动辨识语言;当双语混杂时,需要指定语言,否则会有漏译情况;
+    from_lang_bd = "zh"  # 百度: 'auto', 微软: None; 自动辨识语言;当双语混杂时,需要指定语言,否则会有漏译情况;
+    to_lang = "en"  # 百度:( 中文: zh;文言文: wyw;日本: jp; --> 伊朗语: ir; 波斯语);微软: "zh-Hans"中文简体
+    ExcelPath = r"E:/Working Documents/Eastcom/Russia/Murom/Terminal/T8200 SKD BOM.xlsx"
+    readSheet = "2Murom"
+    readHeader = 4
     readStartRow = None
     readCol = 3  # 也可以用列表读取多列
-    nrows = 60
-    write2Sheet = 'Sheet2'
+    nrows = 20
+    write2Sheet = "Sheet3"
     write2Row = 2
     write2Col = 1
 
     config_path = r"e:/Python_WorkSpace/config/baidu_OpenAPI.ini"
     MSconfig_path = r"E:/Python_WorkSpace/config/Azure_Resources.ini"
 
-    baidu_appid, baidu_appkey = config_read(config_path, section="baidu_OpenAPI", option1='appid', option2='appkey')
-    ms_subscribeKey = config_read(MSconfig_path, section="MS_translation", option1="subscription_key", option2=None)
+    baidu_appid, baidu_appkey = config_read(
+        config_path, section="baidu_OpenAPI", option1="appid", option2="appkey"
+    )
+    ms_subscribeKey = config_read(
+        MSconfig_path,
+        section="MS_translation",
+        option1="subscription_key",
+        option2=None,
+    )
 
-    # 百度翻译引擎:
+    # # 百度翻译引擎:
     ExcelColumnTranslate(translate_engine=translate_engine.baidu, baidu_appid=baidu_appid, baidu_appkey=baidu_appkey,
-                         from_lang=from_lang, to_lang=to_lang, ExcelPath=ExcelPath,
+                         from_lang=from_lang_bd, to_lang=to_lang, ExcelPath=ExcelPath,
                          readSheet=readSheet, readHeader=readHeader, readStartRow=readStartRow, readCol=readCol,
                          nrows=nrows,
                          write2Sheet=write2Sheet, write2Row=write2Row, write2Col=write2Col)
-    # 微软翻译引擎:
+    # # 微软翻译引擎:
     ExcelColumnTranslate(translate_engine=translate_engine.microsoft, ms_subscribeKey=ms_subscribeKey,
                          baidu_appid=baidu_appid,
-                         baidu_appkey=baidu_appkey, from_lang=None, to_lang=to_lang,
+                         baidu_appkey=baidu_appkey, from_lang=from_lang_ms, to_lang=to_lang,
                          ExcelPath=ExcelPath,
                          readSheet=readSheet, readHeader=readHeader, readStartRow=readStartRow, readCol=readCol,
                          nrows=nrows,
@@ -247,3 +320,5 @@ if __name__ == "__main__":
 
     # 从效果上看, 百度翻译比微软翻译速度慢很多,但是效果似乎在列表类中文文本上,似乎比微软要好一些;可能百度在理解中文上更胜一筹.
     print("翻译完成!")
+
+
