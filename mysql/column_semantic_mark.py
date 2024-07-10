9e3e9d37975bc4cb89a19a920f08901db850e490
D.T <dancingonmoon@msn.com>
2024-07-10 17:31:03 +0800

    improve

--- mysql/column_semantic_mark.py
+++ mysql/column_semantic_mark.py
@@ -28,17 +28,15 @@ def semantic_batch_search(
     3) Dataset.add_faiss_index,生成faiss索引;
     3) query 送入embedding-2模型,获得长度1024的向量;
     4) Dataset.get_nearest_examples,获得最佳的k个dataset的样本;或者serach()获得最佳的k个dataset的样本的位置
-    :param:
-        client: object; zhipuAI client (已经送入API_KEY)
-        query: Union[str,List[str], np.ndarray]; 欲搜索的关键词或者句子,或者向量,或者字符串列表;query不应有空字符串出现
-        sentences: 列表或者字典,送入embedding-2模型,获得长度1024的向量列表;key:列名;value:文本;不应有空字符串出现
-        embedding_txt_key: str; sentences字典中用于embedding的key的名称,该key下的values将用于embedding并用于语义搜索;当sentences为list,可为None;sentence为字典时,缺省第一列可设为None
-        embedded_vector_key: str: 如果预存有向量化后的向量空间,此处为其key名;
-        k: int; 返回最相似的句子数量
-        return_indices_samples: bool;是否返回检索后在序列中的位置;True: 返回检索位置;False:返回nearest_samples
-    :return:
-        scoresList[List[float], nearest_examples中的text; 得分,以及对应的句子 (score越小,越佳),类型分别为: List[List[float], List[dict],
-        或者 scores, indices, 类型: List[List[float], List[int]
+    :param client: object; zhipuAI client (已经送入API_KEY)
+    :param query: Union[str,List[str], np.ndarray]; 欲搜索的关键词或者句子,或者向量,或者字符串列表;query不应有空字符串出现
+    :param sentences: 列表或者字典,送入embedding-2模型,获得长度1024的向量列表;key:列名;value:文本;不应有空字符串出现
+    :param embedding_txt_key: str; sentences字典中用于embedding的key的名称,该key下的values将用于embedding并用于语义搜索;当sentences为list,可为None;sentence为字典时,缺省第一列可设为None
+    :param embedded_vector_key: str: 如果预存有向量化后的向量空间,此处为其key名;
+    :param k: int; 返回最相似的句子数量
+    :param return_indices_samples: bool;是否返回检索后在序列中的位置;True: 返回检索位置;False:返回nearest_samples
+    :return: scoresList[List[float], nearest_examples中的text; 得分,以及对应的句子 (score越小,越佳),类型分别为: List[List[float], List[dict],
+            或者 scores, indices, 类型: List[List[float], List[int]
     """
     sentences_txt = []
     sentences_vec = []
@@ -95,7 +93,7 @@ def semantic_batch_search(
     if (
         isinstance(query, str)
         or (isinstance(query, np.ndarray) and np.ndim(query) == 1)
-        or (isinstance(query, list) and all(isinstance(x, (int, float)) for x in query) )
+        or (isinstance(query, list) and all(isinstance(x, (int, float)) for x in query))
     ):
         # 当query 字符串时 向量化
         if isinstance(query, str):
@@ -218,6 +216,84 @@ def mark_semantic_index(
     return df1
 
 
+def df2Excel_SemanticIndex(
+    zhipuai_client,
+    df1_excelPath,
+    df2_excelPath,
+    df1_outPath,
+    df2_outPath,
+    sheet_name="semantic",
+):
+    """
+    读取两个Excel文件,df1,df2,分别代表source和target,通过zhipuai_client,对df1的"专业名称"列与df2的"专业名称"列进行语义比较,并Mark匹配index;然后分别写入到两个Excel文件中;
+    1) 读取两个Excel成DataFrame;
+    2) 语义比较,并Mark匹配index;
+    3) 对每个df中每个学校代号,语义比较,并Mark匹配index;两张表,互相比较;
+    4) 写入指定的excel文件,指定的sheet表中,
+    :param zhipuai_client:
+    :param df1_excelPath:
+    :param df2_excelPath:
+    :param df1_outPath:
+    :param df2_outPath:
+    :return: 写入excel文件,无返回;
+    """
+    path_list = [df1_excelPath, df2_excelPath , df1_outPath,    df2_outPath,]
+    # 检查文件是否存在，并且具有写权限
+    for path in path_list:
+        if not os.path.exists(path):
+            print(f"{path},文件不存在")
+            return
+        if not os.access(path, os.W_OK):
+            print(f"{path},无写权限.")
+            return
+    # 检查xlsx文件是否已经有临时文件打开,从而判断是否EXCEL文件已经打开:
+    path_list = [df1_outPath,df2_outPath]
+    for path in path_list:
+        dirname, filename = os.path.split(path)
+        filename = "~$".join(["", filename])
+        temp_path = os.path.join(dirname, filename)
+        if os.path.exists(temp_path):
+            print(f"{path},文件已经打开,禁止写入.")
+            return
+
+    # 取Excel文件
+    df1 = pd.read_excel(df1_excelPath)
+    df2 = pd.read_excel(df2_excelPath)
+    # 对每个df中每个学校代号,语义比较,并Mark匹配index;两张表,互相比较;
+    df1_university_arr = df1["学校代号"].unique()
+    df2_university_arr = df2["学校代号"].unique()
+    university_arr = pd.unique(np.r_[df1_university_arr, df2_university_arr])
+
+    start_time = time.time()
+    for u in university_arr:
+        if u in df1_university_arr:  # 排除不存在的univeristy导致query为[]
+            df_query = mark_semantic_index(
+                df1,
+                df2,
+                university_code=u,
+                zhipuai_client=zhipuai_client,
+                index_semantic_column=f"index_{df1_shortname}Q{df2_shortname}L",
+                score_semantic_column=f"score_{df1_shortname}Q{df2_shortname}L",
+            )
+            print(f"{df1_shortname}->{df2_shortname}: university_code:{u}, completed.")
+        if u in df2_university_arr:
+            df_list = mark_semantic_index(
+                df2,
+                df1,
+                university_code=u,
+                zhipuai_client=zhipuai_client,
+                index_semantic_column=f"index_{df2_shortname}Q{df1_shortname}L",
+                score_semantic_column=f"score_{df2_shortname}Q{df1_shortname}L",
+            )
+            print(f"{df2_shortname}->{df1_shortname}: university_code:{u}, completed.")
+
+    time_assumed = time.time() - start_time
+    print(f"{df1_shortname}<->{df2_shortname} both spent {time_assumed/60:.2f}min ")
+    # sheetName = "semantic"
+    df_query.to_excel(df1_outPath, sheet_name=sheet_name, index=False, na_rep="")
+    df_list.to_excel(df2_outPath, sheet_name=sheet_name, index=False, na_rep="")
+
+
 if __name__ == "__main__":
     # 准备zhipuai client:
     config_path_zhipuai = r"e:/Python_WorkSpace/config/zhipuai_SDK.ini"
@@ -233,43 +309,16 @@ if __name__ == "__main__":
     df1_excelPath = excel_path_base.format(df1_shortname)
     df2_excelPath = excel_path_base.format(df2_shortname)
     df1_outPath = os.path.splitext(df1_excelPath)[0] + ".xlsx"
-    df2_outPath = os.path.splitext(df2_excelPath)[0] + '.xlsx'
-    # 读取Excel文件
-    df1 = pd.read_excel(df1_excelPath)
-    df2 = pd.read_excel(df2_excelPath)
-    # 语义比较,并Mark匹配index
-    df1_university_arr = df1["学校代号"].unique()
-    df2_university_arr = df2["学校代号"].unique()
-    university_arr = pd.unique(np.r_[df1_university_arr, df2_university_arr])
+    df2_outPath = os.path.splitext(df2_excelPath)[0] + ".xlsx"
 
-    start_time = time.time()
-    for u in university_arr:
-        if u in df1_university_arr: # 排除不存在的univeristy导致query为[]
-            df_query = mark_semantic_index(
-            df1,
-            df2,
-            university_code=u,
-            zhipuai_client=zhipuai_client,
-            index_semantic_column=f"index_{df1_shortname}Q{df2_shortname}L",
-            score_semantic_column=f"score_{df1_shortname}Q{df2_shortname}L",
-        )
-            print(f"{df1_shortname}->{df2_shortname}: university_code:{u}, completed.")
-        if u in df2_university_arr:
-            df_list = mark_semantic_index(
-            df2,
-            df1,
-            university_code=u,
-            zhipuai_client=zhipuai_client,
-            index_semantic_column=f"index_{df2_shortname}Q{df1_shortname}L",
-            score_semantic_column=f"score_{df2_shortname}Q{df1_shortname}L",
-        )
-            print(f"{df2_shortname}->{df1_shortname}: university_code:{u}, completed.")
-
-    time_assumed = time.time()-start_time
-    print(f"{df1_shortname}<->{df2_shortname} both spent {time_assumed/60:.2f}min ")
-    sheetName = "semantic"
-    df_query.to_excel(df1_outPath, sheet_name=sheetName, index=False, na_rep="")
-    df_list.to_excel(df2_outPath, sheet_name=sheetName, index=False, na_rep="")
+    # # df1,df2,相互检索对方,对每个大学,匹配专业代号,写入excel文件:
+    # df2mark_Excelout(zhipuai_client=zhipuai_client,
+    #                  df1_excelPath=df1_excelPath,
+    #                  df2_excelPath=df2_excelPath,
+    #                  df1_outPath=df1_outPath,
+    #                  df2_outPath=df2_outPath,
+    #                  sheet_name="semantic"
+    #                  )
 
     # 发现:
     # 2022年与2021年语义比较:
@@ -280,27 +329,73 @@ if __name__ == "__main__":
 
     # # Step 0: df1与df2相互进行query并生成index,score列; 即: a)去年做query, 生成index_OldQ_NewL, score_OldQ_NewL;b)今年做query,生成index_NewQ_OldL
     # # Step 1: 去年(旧年)做Query的index时, 今年(新年)有同学校的某一个专业为去年(旧年)合并而来;解决: groupby(semantic_index!=-1).agg()
-    # index_semantic_column = f"index_{df1_shortname}Q{df2_shortname}L"
-    # score_semantic_column = f"score_{df1_shortname}Q{df2_shortname}L"
-    #
-    # combine = pd.read_excel(df1_outPath, nrows=100)
-    #
-    # combine[index_semantic_column] = combine[index_semantic_column].apply(lambda x: np.nan if x==-1 else x)
-    # grouped =combine.groupby(by=["学校代号",index_semantic_column],sort=False,dropna=True)
-    # # 使用字典对每列定义函数:
-    # result = grouped.agg({'学校名称': lambda x: x,
-    #                       '专业名称': lambda x: x,
-    #                       '专业代号': lambda x: x,
-    #                       '计划数': lambda x: int(x.sim()),
-    #                       '分数线': lambda x: int(x.mean()),
-    #                       '位次': lambda x : int(x.mean()),
-    #                      score_semantic_column : 'min'}).reset_index()
+    index_semantic_column = f"index_{df2_shortname}Q{df1_shortname}L"
+    score_semantic_column = f"score_{df2_shortname}Q{df1_shortname}L"
+
+    df1 = pd.read_excel(df1_outPath)
+    df2 = pd.read_excel(
+        df2_outPath,
+    )
+    # 增加一列还原index_semantic对应的专业名称
+    def recover_major(df1_x:pd.Series, df1_index_semantic_column, df2:pd.DataFrame, ):
+        """
+        将df1的每一行:df1_x,读出df1_index_semantic, 找到df2对应学校代号下的指定index的专业名称,再输出
+        :param df1_x: pd.Series; df1的行
+        :param df2: pd.DataFrame; df2
+        :param index_semantic_column: df1对应的df1_index_semantic_column列名
+        :return: str; 专业名称
+        """
+        major_arr = df2[df2['学校代号'] == df1_x['学校代号']]['专业名称'].values #pd.Series转换成ndarry
+        if major_arr.size == 0 or major_arr is None:
+            major = ""
+        else:
+            index = df1_x[df1_index_semantic_column]
+            if index != -1 and index != np.NAN:
+                major = major_arr[index]
+            else:
+                major = ''
+        return major
+    df2[f'{df1_shortname}专业名称'] = df2.apply(recover_major,args=(df1,),axis=1)
+    df1[f'{df2_shortname}专业名称'] = df1.apply(recover_major,args=(df2,),axis=1)
+
+    # print(df1, df2)
+
+    df2[index_semantic_column] = df2[index_semantic_column].apply(
+        lambda x: np.nan if x == -1 else x
+    ) # -1转化为Nan,因为agg()不对Nan进行计算
+    grouped = df2.groupby(
+        by=["学校代号", index_semantic_column], sort=False, dropna=False
+    ) # dropna=False 保留Nan行
+    # 使用字典对每列定义函数:
+    result = grouped.agg(
+        {
+            "学校名称": lambda x: x,
+            "专业名称": lambda x: x,
+            "专业代号": lambda x: x,
+            "计划数": lambda x: int(x.sum()),
+            "分数线": lambda x: int(x.mean()),
+            "位次": lambda x: int(x.mean()),
+            score_semantic_column: "min",
+        }
+    ).reset_index()
     # #或使用,pd.NamedAgg: 优点,可以直接定义更改后的列名
     # result_ = grouped.agg(**{'平均分数线':('分数线', 'mean'),
     #                          '平均位次':('位次','mean')})
-    # print(result)
+    # 列排序:
+    columns = [
+        "学校代号",
+        "学校名称",
+        "专业代号",
+        "专业名称",
+        "计划数",
+        "分数线",
+        "位次",
+        index_semantic_column,
+        score_semantic_column,
+    ]
+    result = result[columns[:]]
+    sheet_name = 'combine'
+    result.to_excel(df2_outPath, sheet_name=sheet_name, index=False, na_rep="")
     #
     # # Step2: 今年(新年)做Query,去年(旧年)做List,今年有几个专业是由去年某个专业拆解而来;解决: pd.concat()
     # pd.merge(df1, df2, left_on=['学校代号',index_semantic_column], right_on=['学校代号',index_semantic_column], how='outer')
-
-
